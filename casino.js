8166e1d2bac7adb9d9006a68b2f46677768f123a
Alpagadesalpes <robin.silvert@gmail.com>
2017-04-16 15:47:08 +0200

    modification ajout d'une 4 eme case

--- casino.js
+++ casino.js
@@ -2,6 +2,7 @@ function runSlots() {
     var One;
     var Two;
     var Three;
+    var Four;
     var images = ["https://upload.wikimedia.org/wikipedia/commons/a/ab/Naipe_ouros.png", "https://upload.wikimedia.org/wikipedia/commons/a/a0/Naipe_copas.png", "https://upload.wikimedia.org/wikipedia/commons/5/5c/Naipe_espadas.png"
  , "https://upload.wikimedia.org/wikipedia/commons/8/8a/SuitClubs.svg"];
     
@@ -10,17 +11,20 @@ function runSlots() {
     One = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
     Two = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
     Three = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    Four = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
     $('.result').html('YOU LOSE');
     $($('.x')[0]).html('<img src = "' + images[One - 1] + '">');
     $($('.x')[1]).html('<img src = "' + images[Two - 1] + '">');
     $($('.x')[2]).html('<img src = "' + images[Three - 1] + '">');
-    if (One !== Two || Two !== Three) {
+    $($('.x')[3]).html('<img src = "' + images[Four - 1] + '">');
+    if (One !== Two || Two !== Three || Three!== Four) {
         return null;
     }
-    if (One !== undefined && Two !== undefined && Three !== undefined) {
+    if (One !== undefined && Two !== undefined && Three !== undefined && Four !== undefined) {
         $('.result').html(One);
         $('.result').append(' ' + Two);
         $('.result').append(' ' + Three);
+        $('.result').append(' ' + Four);
     }
     return $('.result').html('YOU WIN');
 }
